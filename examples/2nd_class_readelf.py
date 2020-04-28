71b9b414aebc54dcf8cf6f7490ad2cfbd7a7d7a2
Dee Dee <>
2020-04-28 09:46:39 +0200

    simple example

---
+++ examples/2nd_class_readelf.py
@@ -0,0 +1,31 @@
+
+import argparse
+
+import deedee.elf as elf
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser('A dumb Python readelf')
+    path   = parser.add_argument('path')
+    args   = parser.parse_args()
+
+    with open(args.path, 'rb') as f:
+        data = f.read()
+
+    e = elf.Elf(data)
+
+    print('machine:', e.machine.name)
+    print('entry:', hex(e.entry))
+
+    print(f'segments ({len(e.segments)}):')
+    for n, segment in enumerate(e.segments):
+        print(f'    - {n}: {segment.type.name}')
+
+    print(f'sections ({len(e.sections)}):')
+    for name, section in e.sections.items():
+        print(f'    - {name}: {section.type.name}')
+
+    print('section to segment mappings:')
+    for n, segment in enumerate(e.segments):
+        print(f'    - {n}: {", ".join(segment.sections.keys())}')
+
